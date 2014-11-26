1daa33d3ba921de27e66b9238a7d767fa1b3fffd
Bibek Shrestha <bibekshrestha@gmail.com>
2014-11-26 15:39:58 +0100

    Move to videojs complete

--- js/components/VideoPlayer.react.js
+++ js/components/VideoPlayer.react.js
@@ -2,30 +2,6 @@ var React = require('react');
 var VAActions = require('../actions/VAActions');
 var VideoPlayerStore = require('../stores/VideoPlayerStore');
 
-function initializeVideoPlayer(video) {
-  if (video !== null) {
-    $("#videoplayer").flowplayer({
-      playlist: [
-        [
-          { webm: video.get('url') }
-        ]
-      ],
-      ratio: 3/4,   // video with 4:3 aspect ratio
-      splash: true  // a splash setup
-    });
-    
-    var api = $("#videoplayer").flowplayer();
-    api.bind('progress', function(e, api, time) {
-      VAActions.updateVideoPlayerProgress({time: time});
-    });
-  }
-}
-
-function pauseVideoPlayer() {
-  var api = $("#videoplayer").flowplayer();
-  api.pause();
-}
-
 var VideoPlayer = React.createClass({
   getInitialState: function() {
     return {video: null}
@@ -33,31 +9,26 @@ var VideoPlayer = React.createClass({
   componentDidMount: function() {
     VideoPlayerStore.addChangeListener(this._onChange);
     VideoPlayerStore.addPlayerListener(this._onPlayerEventCallback);
+    this.initializeVideoPlayer();
   },
   componentDidUnmount: function() {
     VideoPlayerStore.removeChangeListener(this._onChange);
     VideoPlayerStore.removePlayerListener(this._onPlayerEventCallback);
   },
-  render: function() {
-    if (this.state.video !== null) {
-      return (
-        <div>
-          <h4>Video Player</h4>
-          <h3>{this.state.video.get('title')}</h3>
-          <div id="videoplayer"></div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h4>Video Player</h4>
-          <div>Please select a video.</div>
-        </div>
-      );
-    }
-  },
   componentDidUpdate: function() {
-    initializeVideoPlayer(this.state.video);
+    this.updateVideoPlayer();
+  },
+  render: function() {
+    
+    var title = (this.state.video !== null) ? this.state.video.get('title') : "Please select a video";
+    return (
+      <div>
+        <h4>Video Player</h4>
+        <h3>{title}</h3>
+        <video id="videoplayer" className="video-js vjs-default-skin vjs-big-play-centered" controls 
+          width="480" height="360"></video>
+      </div>
+    );
   },
   
   _onChange: function() {
@@ -67,10 +38,40 @@ var VideoPlayer = React.createClass({
   _onPlayerEventCallback: function(type) {
     switch (type) {
       case 'pause':
-        pauseVideoPlayer();
+        this.pauseVideoPlayer();
         break;
         
     }
+  },
+  
+  initializeVideoPlayer: function() {
+    videojs('videoplayer', {}, function() {
+      console.log("Video Player Initialized!!");
+    });
+  },
+  
+  updateVideoPlayer: function() {
+    var video = this.state.video;
+    if (video !== null) {
+      var player = videojs('videoplayer').src(video.get('url'));
+      
+      // A closure to save last_time value
+      var timeUpdateCallback = (function(time_gap) {
+        var last_time = 0
+        return function() {
+          if (this.currentTime() - last_time > time_gap) {
+            last_time = this.currentTime();
+            VAActions.updateVideoPlayerProgress({time: last_time});
+          }
+        }
+      })(0.4); // only update at a 0.4 second interval
+      
+      player.on('timeupdate', timeUpdateCallback);
+    }
+  },
+  
+  pauseVideoPlayer: function() {
+    videojs('videoplayer').pause();
   }
 });
 
